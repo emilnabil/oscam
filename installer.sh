9ed41b8e5c565235fc02a99942731e95049bae39
emil nabil <elbrinselomda@yahoo.com>
2021-09-17 23:09:09 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -13,21 +13,22 @@ OSVERSION='11.696'
 OSPACKAGE='oscam'
 NCVERSION='11690'
 NCPACKAGE='ncam'
+OPKGINSTAL='opkg install --force-overwrite'
 MY_URL='https://raw.githubusercontent.com/emilnabil/oscam/main'
 
 ####################
 echo "==============================================================================================================="
 echo "Insallling Emu Oscam - ncam  Please Wait "
-   wait 2;
+   wait 2
             wget $MY_URL/${OSPACKAGE}_${OSVERSION}_all.ipk -qP $TMPDIR
             $OPKGINSTAL $TMPDIR/${OSPACKAGE}_${OSVERSION}_all.ipk
         
-            echo "Insallling Oscam plugin Please Wait ......"
-            wget $MY_URL/${NCPACKAGE}_${NCVERSION}_all.deb -qP $TMPDIR
+            echo "Insallling ncam plugin Please Wait ......"
+            wget $MY_URL/${NCPACKAGE}_${NCVERSION}_all.ipk -qP $TMPDIR
           $OPKGINSTAL $TMPDIR/${NCPACKAGE}_${NCVERSION}_all.ipk
         
     ###########################
-rm -rf $TMPDIR/${OSPACKAGE}_{VERSION}_all.ipk
+rm -rf $TMPDIR/${OSPACKAGE}_{OSVERSION}_all.ipk
 echo **********************************************************"
 rm -rf $TMPDIR/${NCPACKAGE}_{NCVERSION}_all.ipk MY_EM="======================================================================================================="
  echo "******************"
@@ -42,6 +43,8 @@ rm -rf $TMPDIR/${NCPACKAGE}_{NCVERSION}_all.ipk MY_EM="=========================
    wait 2;
     sleep 2;
   exit    
+
+
 
 
 
