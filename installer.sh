d4920dcd9ebf141df4fa344fb8935949d8645329
emil nabil <elbrinselomda@yahoo.com>
2021-09-19 18:07:16 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # ###########################################
-# SCRIPT : DOWNLOAD AND INSTALL OSCAM ###
+# SCRIPT :              DOWNLOAD AND INSTALL OSCAM + NCAM ###
 # ###########################################
 #
 # Command: wget https://raw.githubusercontent.com/emilnabil/oscam/main/installer.sh -O - | /bin/sh
@@ -18,26 +18,76 @@ MY_URL='https://raw.githubusercontent.com/emilnabil/oscam/main'
 
 ####################
 echo "==============================================================================================================="
+#  Image Checking  #
+
+if which opkg > /dev/null 2>&1; then
+    STATUS='/var/lib/opkg/status'
+    OSTYPE='Opensource'
+    OPKG='opkg update'
+    OPKGINSTAL='opkg install'
+    OPKGREMOV='opkg remove --force-depends'
+else
+    STATUS='/var/lib/dpkg/status'
+    OSTYPE='DreamOS'
+    OPKG='apt-get update'
+    OPKGINSTAL='apt-get install'
+    OPKGREMOV='apt-get purge --auto-remove'
+    DPKINSTALL='dpkg -i --force-overwrite'
+fi
+############################
+###   remove old plugins ###
+opkg remove enigma2-softcams-cccam-all-images
+wait
+opkg remove enigma2-plugin-softcams-ncam
+wait
+apt-get purge --auto-remove enigma2-plugin-softcams-ncam-osdreambox
+wait
+opkg remove --force-depends enigma2-softcams-oscam-all-images
+wait
+#############################
 echo "Insallling Emu Oscam - ncam  Please Wait "
    wait 2
-            wget $MY_URL/${OSPACKAGE}_${OSVERSION}_all.ipk -qP $TMPDIR
-            $OPKGINSTAL $TMPDIR/${OSPACKAGE}_${OSVERSION}_all.ipk
-        
-            echo "Insallling ncam plugin Please Wait ......"
-            wget $MY_URL/${NCPACKAGE}_${NCVERSION}_all.ipk -qP $TMPDIR
-          $OPKGINSTAL $TMPDIR/${NCPACKAGE}_${NCVERSION}_all.ipk
-        
+cd /tmp
+set -e
+            wget "https://raw.githubusercontent.com/emilnabil/oscam/main/ncam_11.90_all.deb"
+wait 1
+   wget
+"https://raw.githubusercontent.com/emilnabil/oscam/main/ncam_11.90_all.ipk"
+ wait 1
+  wget
+"https://raw.githubusercontent.com/emilnabil/oscam/main/oscam-revcamV2_11.696_all.ipk"
+ wait 1
+ wget
+"https://raw.githubusercontent.com/emilnabil/oscam/main/oscam-supcam_11.696_all.ipk"
+  wait 1
+ wget
+"https://raw.githubusercontent.com/emilnabil/oscam/main/oscam_11.696_all.deb"
+ wait 1
+ wget
+"https://raw.githubusercontent.com/emilnabil/oscam/main/oscam_11.696_all.ipk"
+wait 2
+cd ..
+  opkg install --force-overwrite /tmp/*.ipk
+wait 1
+    apt-get update && dpkg -i --force-overwrite /tmp/*.deb; apt-get install -f -y
+echo "======================================================================"
     ###########################
-rm -f $TMPDIR/${OSPACKAGE}_{OSVERSION}_all.ipk
-echo "**********************************************************"
-rm -f $TMPDIR/${NCPACKAGE}_{NCVERSION}_all.ipk 
+rm -rf /tmp/*.ipk
+wait
+rm -rf /tmp/*.deb
+wait 1
 echo "======================================================================================================="
- echo "    UPLOADED BY       EMIL_NABIL "   
-
-  echo " =============================================================================="
+ echo "    UPLOADED BY  >>>>   EMIL_NABIL "   
+   echo "
+  ======      =   =        =   =
+  =          =  =  =       =   =
+  ======    =   =   =      =   =
+  =        =         =     =   = 
+  ======  =           =    =   ===== "
    wait 2;
     sleep 2;
   exit    
+
 
 
 
