1d1532650eed40a7cac5814a3724b0b64ccb9645
emil nabil <elbrinselomda@yahoo.com>
2021-09-22 21:01:07 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,103 +1,96 @@
 #!/bin/sh
-# ###########################################
-echo "SCRIPT : DOWNLOAD AND INSTALL ALL EMU ( CCCAM - OSCAM - OSSUPCAM - OS REVCAM )"
-# ###########################################
-#
-# Command: wget https://raw.githubusercontent.com/emilnabil/oscam/main/installer.sh -O - | /bin/sh
-#
-# ###########################################
 
+# 
+# SCRIPT : DOWNLOAD AND INSTALL EMU OSCAM #
+# ======================================================================================
+# Command: wget https://raw.githubusercontent.com/emilnabil/oscam/main/installer.sh -O - | /bin/sh #
+# ======================================================================================
+
+# MY CONFIG SCRIPT #
 ###########################################
-# Configuration  My Script #
-TMPDIR='/tmp'
-VERSIONCCCAM='2.3.9'
-VERSIONNCAM='11.90'
-VERSIONOSCAM='11.696'
-VERSIONOSSUPCAM='11.696'
-VERSIONOSREVCAM='11.696'
-PACKAGECCCAM='cccam-all-images'
-PACKAGENCAM='ncam'
-PACKAGEOSCAM='oscam'
-PACKAGEOSSUPCAM='oscam-supcam'
-PACKAGEOSREVCAM='oscam-revcamV2'
-MY_URL='https://raw.githubusercontent.com/emilnabil/oscam/main/'
+PACKAGE_DIR='oscam/main'
+MY_IPK="oscam_11.696_all.ipk"
+MY_DEB="oscam_11.696_all.deb"
 
-####################
-#  Image Checking  #
-if which opkg > /dev/null 2>&1; then
-    STATUS='/var/lib/opkg/status'
-    OSTYPE='Opensource'
-    OPKGINSTAL='opkg install'
-    OPKGREMOV='opkg remove --force-depends'
+
+######################################################################################
+# Auto ... Do not change
+######################################################################################
+
+# Decide : which package ?
+MY_MAIN_URL="https://raw.githubusercontent.com/emilnabil/"
+if which dpkg > /dev/null 2>&1; then
+	MY_FILE=$MY_DEB
+	MY_URL=$MY_MAIN_URL${PACKAGE_DIR}/${MY_DEB}
 else
-    STATUS='/var/lib/dpkg/status'
-    OSTYPE='DreamOS'
-    OPKGINSTAL='apt-get install'
-    OPKGREMOV='apt-get purge --auto-remove'
-    DPKINSTALL='dpkg -i --force-overwrite'
+	MY_FILE=$MY_IPK
+	MY_URL=$MY_MAIN_URL${PACKAGE_DIR}/${MY_IPK}
 fi
+MY_TMP_FILE="/tmp/"$MY_FILE
 
-##################################
-######################
-#  Remove Old Emu #
-$OPKGREMOV enigma2-softcams-cccam-all-images
-wait
-$OPKGREMOV enigma2-plugin-softcams-ncam-mips
-wait
-$OPKGREMOV enigma2-softcams-oscam-all-images
-wait
- echo "             Removed Old Emu "
+echo ''
+echo '************************************************************'
+echo '**                         STARTED                        **'
+echo '************************************************************'
+#            # WRITE Script By  Biko_73 #                
+# AND MODIFY Script To Work Emu OScam BY    
+  EMIL_NABIL  #
+echo "************************************************************"
+echo ''
 
-#####################
-###################
-#             Installing #
-if [ $OSTYPE = "Opensource" ]; then
-    echo "Insalling EMU Please Wait ......"
-    wget $MY_URL/${PACKAGECCCAM}_${VERSIONCCCAM}_all.ipk -qP $TMPDIR
-      wait
-    wget $MY_URL/${PACKAGENCAM}_${VERSIONNCAM}_all.ipk -qP $TMPDIR
-    wait
-    wget $MY_URL/${PACKAGEOSCAM}_${VERSIONOSCAM}_all.ipk -qP $TMPDIR
-    wait
-    wget $MY_URL/${PACKAGEOSSUPCAM}_${VERSIONOSSUPCAM}_all.ipk -qP $TMPDIR
-    wait
-    wget $MY_URL/${PACKAGEOSREVCAM}_${VERSIONOSREVCAM}_all.ipk -qP $TMPDIR
-    wait
-    $OPKGINSTAL $TMPDIR/*.ipk
-else
-    echo "Insallling EMU Please Wait ......"
-    wget $MY_URL/${PACKAGECCCAM}_${VERSIONCCCAM}_all.deb -qP $TMPDIR
-    wait
-    wget $MY_URL/${PACKAGENCAM}_${VERSIONNCAM}_all.deb -qP $TMPDIR
-    wait
-   wget $MY_URL/${PACKAGEOSCAM}_${VERSIONOSCAM}_all.deb -qP $TMPDIR
-          ### instaling ###
-    wait 
-    $DPKINSTALL $TMPDIR/${PACKAGECCCAM}_${VERSIONCCCAM}_all.deb; $OPKGINSTAL -f -y
-    wait
-      $DPKINSTALL $TMPDIR/${PACKAGENCAM}_${VERSIONNCAM}_all.deb; $OPKGINSTAL -f -y
-    wait
-     $DPKINSTALL $TMPDIR/${PACKAGEOSCAM}_${VERSIONOSCAM}_all.deb; $OPKGINSTAL -f -y
-fi
+# Remove previous file (if any)
+rm -f $MY_TMP_FILE > /dev/null 2>&1
 
-##################################
-# Remove previous files  #
- rm -f $TMPDIR/*.ipk
- rm -f $TMPDIR/*.deb
-sleep 1; 
-clear
+# Download package file
+EM='============================================================='
+echo $EM
+echo 'Downloading '$MY_FILE' ...'
+echo $EM
+echo ''
+wget -T 2 $MY_URL -P "/tmp/"
+
+# Check download
+if [ -f $MY_TMP_FILE ]; then
+	# Install
+	echo ''
+	echo $EM
+	echo 'Installation started'
+	echo $EM
+	echo ''
+	if which dpkg > /dev/null 2>&1; then
+		apt-get install --reinstall $MY_TMP_FILE -y
+	else
+		opkg install --force-reinstall $MY_TMP_FILE
+	fi
+	MY_RES=$?
+
+	# Res
+	echo ''
+	echo ''
+	if [ $MY_RES -eq 0 ]; then
+		echo "   >>>>   SUCCESSFULLY INSTALLED   <<<<"
+		echo ''
+		echo "   >>>>         RESTARING         <<<<"
+		if which systemctl > /dev/null 2>&1; then
+			sleep 2; systemctl restart enigma2
+		else
+			init 4; sleep 4; init 3;
+		fi
+	else
+		echo "   >>>>   INSTALLATION FAILED !   <<<<"
+	fi;
+	echo ''
+	echo '**************************************************'
+	echo '**                   FINISHED                   **'
+	echo '**************************************************'
+	echo ''
 echo ""
-echo "***********************************************************************"
-echo "**                                                                    *"
-echo "**                  CONGRATULATIONS                            *"
-echo "**                                                                    *"
-echo " UPLOADED BY  >>>> EMIL_NABIL   "   
-   echo "
-  888888======8===8========8===8
-  8==========8=8=8=8=======8===8
-  888888====8===8===8======8===8   
-  8========8=========8=====8===8 
-  888888==8===========8====8===888888 "
-   wait 
-    sleep 2
-  exit    
+   wait 2;   
+	exit 0
+else
+	echo ''
+	echo "Download failed !"
+	exit 1
+fi
+
+# 
