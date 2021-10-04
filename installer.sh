87feb23623a38d43738a1c9b4c16b2017f193e20
emil nabil <elbrinselomda@yahoo.com>
2021-10-04 18:09:53 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -3,12 +3,11 @@
 ##setup command=wget https://raw.githubusercontent.com/emilnabil/oscam/main/installer.sh -O - | /bin/sh
 #
 echo " download and install oscam emu "
-
-version=11.696
+version=11.697
 OPKGINSTALL=opkg install --force-overwrite
 MY_URL="https://raw.githubusercontent.com/emilnabil/oscam/main"
-MY_IPK="oscam_11.696_all.ipk"
-MY_DEB="oscam_11.696_all.deb"
+MY_IPK="oscam_11.697_all.ipk"
+MY_DEB="oscam_11.697_all.deb"
 ##############################################################################
 # remove old emu #
 opkg remove enigma2-plugin-softcams-oscam-all-images
@@ -25,14 +24,13 @@ set -e
      wget "$MY_URL/$MY_DEB"
 
  if which dpkg > /dev/null 2>&1; then
-		apt-get install --reinstall oscam_11.696_all.deb -y
+		apt-get install --reinstall oscam_11.697_all.deb -y
 	else
-		opkg install --force-reinstall oscam_11.696_all.ipk
+		opkg install --force-reinstall oscam_11.697_all.ipk
 	fi
 echo "================================="
 set +e
 chmod 755 /usr/bin/oscam-emu
-chmod 755 /usr/bin/OSCam_11.696-r798
 cd ..
 wait
 rm -f /tmp/$MY_IPK
@@ -43,23 +41,18 @@ fi
 		echo "********************************************************************************"
 echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
 sleep 4;
-		echo ". >>>>         RESTARING     <<<<"
-		if which systemctl > /dev/null 2>&1; then
-			sleep 2; systemctl restart enigma2
-		else
-			init 4; sleep 4; init 3;
-		fi
-	else
-		echo "   >>>>   INSTALLATION FAILED !   <<<<"
-	fi;
-	echo ''
-	echo '**************************************************'
-	echo '**                   FINISHED                   **'
-echo "********************************************************************************"
-echo "#########################################################"
-   wait 2;
-    sleep 2;
-  exit    
+	echo '========================================================================================================================='
+###################                                                                                                                  
+echo " your Device Will RESTART NOW " 
+echo "**********************************************************************************"
+wait
+init 4
+init 3
+exit 0
+
+
+
+
 
 
 
