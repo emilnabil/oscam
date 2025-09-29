1e4ca800e937ca9bad13cafe8078310d24305e42
emil-nabil333 <elbrinselomda@yahoo.com>
2025-09-29 12:05:27 +0300

    Add files via upload

--- oscam-emu-levi45.sh
+++ oscam-emu-levi45.sh
@@ -1,61 +1,59 @@
 #!/bin/sh
 
 ##setup command=wget https://raw.githubusercontent.com/emilnabil/oscam/main/oscam-emu-levi45.sh -O - | /bin/sh
-#
-echo " download and install oscam emu "
-version=11.799
-OPKGINSTALL=opkg install --force-overwrite
+
+echo "Downloading and installing oscam emu"
+
 MY_URL="https://raw.githubusercontent.com/emilnabil/oscam/main"
-MY_IPK="enigma2-plugin-softcams-oscam-emu-levi45_11813-802_all.ipk"
-MY_DEB="enigma2-plugin-softcams-oscam-emu-levi45_11813-802_all.deb"
+MY_IPK="enigma2-plugin-softcams-oscam-emu-levi45_all.ipk"
+MY_DEB="enigma2-plugin-softcams-oscam-emu-levi45_all.deb"
+
 ##############################################################################
-# remove old emu #
+# Remove old emu #
+echo "Removing old versions..."
 opkg remove enigma2-plugin-softcams-oscam-all-images
 
 #################################################################################
 
 # Download and install plugin #
-opkg install wget
-cd /tmp 
+echo "Installing..."
+cd /tmp
 
-set -e
-
- if which dpkg > /dev/null 2>&1; then
- wget "$MY_URL/$MY_DEB"
-	dpkg -i --force-overwrite $MY_DEB; apt-get install -f -y
-	set +e
-chmod 755 /usr/bin/oscam-emu
-cd ..
-wait
-rm -f /tmp/$MY_DEB
+if which dpkg > /dev/null 2>&1; then
+    echo "System uses dpkg"
+    wget "$MY_URL/$MY_DEB"
+    if [ -f "$MY_DEB" ]; then
+        dpkg -i --force-overwrite "$MY_DEB"
+        apt-get install -f -y
+        chmod 755 /usr/bin/oscam-emu
+        rm -f "$MY_DEB"
+    else
+        echo "Failed to download: $MY_DEB"
+        exit 1
+    fi
 else
- wget "$MY_URL/$MY_IPK"
-		opkg install --force-reinstall $MY_IPK
-set +e
-chmod 755 /usr/bin/oscam-emu
-cd ..
-wait
-rm -f /tmp/$MY_IPK
-	fi
-echo "================================="
-	if [ $? -eq 0 ]; then
-	echo ">>>>   SUCCESSFULLY INSTALLED <<<<"
+    echo "System uses opkg"
+    wget "$MY_URL/$MY_IPK"
+    if [ -f "$MY_IPK" ]; then
+        opkg install --force-overwrite "$MY_IPK"
+        chmod 755 /usr/bin/oscam-emu
+        rm -f "$MY_IPK"
+    else
+        echo "Failed to download: $MY_IPK"
+        exit 1
+    fi
 fi
-		echo "********************************************************************************"
+
+echo "================================="
+echo ">>>>   SUCCESSFULLY INSTALLED   <<<<"
+echo "********************************************************************************"
 echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
-sleep 4;
-	echo '========================================================================================================================='
-###################                                                                                                                  
-echo ". >>>>         RESTARING     <<<<"
+sleep 2
+
+echo "Restarting..."
 echo "**********************************************************************************"
-wait
+sleep 2
+
 killall -9 enigma2
 exit 0
-
-
-
-
-
-
-
 
